1c10d7e03c45dc4f931052b9f475fe4fbf3b8368
unknown <cliffowino22@gmail.com>
2025-03-03 09:32:04 +0300

    EDA boxplots

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
 
 app_df = pd.read_csv(r'C:\Users\Administrator\work\credit_card_elig\core\credit_card_approval\data\application_record.csv')
@@ -17,8 +18,11 @@ app_df['DAYS_BIRTH'] = abs((app_df['DAYS_BIRTH'] // 365.25).astype(int))
 app_df['DAYS_EMPLOYED'] = app_df['DAYS_EMPLOYED'].apply(emp_edit)
 app_df['DAYS_EMPLOYED'] = app_df['DAYS_EMPLOYED'].astype(int)
 app_df['CNT_FAM_MEMBERS'] = app_df['CNT_FAM_MEMBERS'].astype(int)
-app_df = app_df.rename(columns={'DAYS_BIRTH':'Age','DAYS_EMPLOYED':'Years of Employment','CNT_FAM_MEMBERS':'Family members'})
-print(app_df.describe())
+app_df = app_df.rename(columns={'DAYS_BIRTH':'Age','DAYS_EMPLOYED':'Years of Employment','CNT_FAM_MEMBERS':'Family members',
+                                'AMT_INCOME_TOTAL':'Annual Income','NAME_EDUCATION_TYPE':'Education level',
+                                'OCCUPATION_TYPE':'Occupation','CNT_CHILDREN':'Children','NAME_INCOME_TYPE':'Income Type'})
+print(app_df.sum())
 
-
+print(app_df['Occupation'].isna().sum())
+# plt.show()
 # app_df.info()
--- plots.py
+++ plots.py
@@ -8,3 +8,21 @@ plt.title('Children Distribution')
 plt.xlabel('Children members')
 plt.ylabel('frequency')
 plt.show()
+
+
+ax = sns.boxplot(data={'Family members':app_df['Family members'],'CNT_CHILDREN':app_df['CNT_CHILDREN']},orient='h')
+data={'Family members':app_df['Family members'],'CNT_CHILDREN':app_df['CNT_CHILDREN']}
+for i, category in enumerate(['Family members','CNT_CHILDREN']):
+    Q1 = np.percentile(app_df[category],25)
+    Q3 = np.percentile(app_df[category],75)
+    # print(Q1,Q3)
+    IQR = Q3 - Q1
+    # print(IQR)
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    # print(lower_bound,upper_bound)
+    outliers = app_df[(app_df[category] < lower_bound) | (app_df[category] > upper_bound)][category]
+    print(outliers.sum())
+    # print(app_df.sum())
+    for outlier in outliers:
+        ax.text(outlier, i, f'{outlier:.1f}', ha='left',va='center',color='red',fontsize=12)
